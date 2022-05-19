8ec834d463d421fcd738fb5430f5ffc7dc13b126
Adonis Legon <Adonis.Legon@sovos.com>
2022-05-19 00:34:01 -0400

    minos changes

--- stop-all.sh
+++ stop-all.sh
@@ -1,4 +1,10 @@
-IMAGE_TAG=${1:-1.0.0}
+TAG=${1:-1.0.0}
+PORT=${2:-9999}
+JAVA_OPTS=${3:--Xms256M -Xmx1G}
 SERVICE_NAME=demo-springboot-prometheus
+
+export IMAGE_TAG=$TAG
+export SERVICE_PORT=$PORT
+export JAVA_OPTS=$JAVA_OPTS
 
 docker-compose --project-name $SERVICE_NAME-$IMAGE_TAG down
